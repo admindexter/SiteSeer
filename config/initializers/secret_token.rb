6bad05313ef33ecc77a09f4cd6c5f6055ab91d6d
Dextersmart Harry <dextersmart@gmail.com>
2011-11-15 03:34:24 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siteseer::Application.config.secret_token = 'c16f3cc2ed6e0298902575658f48e7605eb571fd4a3680a041e4bf9ca376d999f2250e60beeb5062ec0fae2c6553e4004ed6ab24eb420aa0b9f8e667122544ac'
